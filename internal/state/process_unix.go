1533443a759f6e9f450fc33a41bc89e8e38a5630
Desta Fajri Dwi Purnomo <106084465+destafajri@users.noreply.github.com>
2026-09-19 17:22:55 +0700

    fix: use kernel advisory locks on Unix

--- internal/state/process_unix.go
+++ internal/state/process_unix.go
@@ -4,13 +4,21 @@ package state
 
 import (
 	"errors"
+	"os"
 	"syscall"
 )
 
-func processAlive(pid int) bool {
-	if pid <= 0 {
-		return false
+func tryLockFile(f *os.File) (bool, error) {
+	err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+	if err == nil {
+		return true, nil
 	}
-	err := syscall.Kill(pid, 0)
-	return err == nil || errors.Is(err, syscall.EPERM)
+	if errors.Is(err, syscall.EWOULDBLOCK) || errors.Is(err, syscall.EAGAIN) {
+		return false, nil
+	}
+	return false, err
+}
+
+func unlockFile(f *os.File) error {
+	return syscall.Flock(int(f.Fd()), syscall.LOCK_UN)
 }
