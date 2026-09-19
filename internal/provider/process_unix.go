5a74f6b344c54d5c701aec7d2e2e3ee56d3668fb
Desta Fajri Dwi Purnomo <106084465+destafajri@users.noreply.github.com>
2026-09-19 17:08:55 +0700

    fix: add Unix provider process-group cleanup

---
+++ internal/provider/process_unix.go
@@ -0,0 +1,24 @@
+//go:build !windows
+
+package provider
+
+import (
+	"errors"
+	"os/exec"
+	"syscall"
+)
+
+func prepareCommand(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+}
+
+func terminateProcessTree(cmd *exec.Cmd) error {
+	if cmd.Process == nil {
+		return nil
+	}
+	err := syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+	if errors.Is(err, syscall.ESRCH) {
+		return nil
+	}
+	return err
+}
